27e6c52a629b22460e25ed46634fa3cf23cb7ac1
Hayoun Kim <hayoun.k.1102@gmail.com>
2025-04-21 22:25:11 -0700

    layout change

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Elaine Y. Shin';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Managing Up';
+export const SITE_DESCRIPTION = `Hey! I'm Elaine, a people leader in start-up marketing. I started this blog to unpack how I think and use my mental models at work.`;
